23b7115d2affe54d8861f7064ee7502422c36a3b
Nadav <nadavmihov@gmail.com>
2026-03-12 10:52:24 +0200

    added pasten cmd

--- src/server.h
+++ src/server.h
@@ -4160,6 +4160,7 @@ void getCommand(client *c);
 void getexCommand(client *c);
 void getdelCommand(client *c);
 void delCommand(client *c);
+void pastenCommand(client *c);
 void delexCommand(client *c);
 void unlinkCommand(client *c);
 void existsCommand(client *c);
